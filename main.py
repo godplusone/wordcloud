c9e3ff5252d1499f80ac63005a265b0290ef42ac
godplusone <shenjiayi@buaa.edu.cn>
2020-10-13 12:12:11 +0800

    no message

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_words(txt):
     # wc.to_file('e:\\26.png')
 
 if __name__ == '__main__':
-    f = open('Accepted Papers.txt', 'r', encoding='utf-8')
+    f = open('Accepted Papers.txt', 'r', encoding='utf-16')
     get_words(f.readlines())
 
 
