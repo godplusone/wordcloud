affa3ac4dc8c0827acd1be14d8b8bf42d3a89859
godplusone <shenjiayi@buaa.edu.cn>
2020-10-13 11:28:34 +0800

    no message

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ from wordcloud import WordCloud
 import imageio
 
 def get_words(txt):
-    #进行分词
+
     list = jieba.cut(txt)
     c = Counter()
 
-    #给分词定义条件进行筛选统计词频
     for x in list:
         if len(x) > 1 and x != '\r\n':
             c[x] += 1
 
-    #打开一个文本将统计好的词频存放进去
-    with open('bb.txt', 'w', 'gbk') as fw:
-        for (k, v) in c.most_common():
-            fw.write(k + ' ' + str(v) + '\n')
-        fw.close()
-    
-    # #绘制词云图
+
+    fw = open('bb.txt', 'w')
+    for (k, v) in c.most_common():
+        fw.write(k + ' ' + str(v) + '\n')
+    fw.close()
+
+
     # pac_mask = imageio.imread(r'e:\1000.png')
     # wc = WordCloud(font_path='simhei.ttf', background_color='white', max_words=2000, mask=pac_mask).fit_words(c)
     # plt.imshow(wc)
@@ -30,10 +29,8 @@ def get_words(txt):
     # wc.to_file('e:\\26.png')
 
 if __name__ == '__main__':
-    #打开需要分词的文本
-    with open('nips2020paperlist.txt', 'r') as f:
-        txt = f.readlines()
-    get_words(txt)
+    f = open('nips2020paperlist.txt', 'r')
+    get_words(f.readlines())
 
 
 
